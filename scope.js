c333933a592792696c1619678a53ce07b72f7e37
eschnei2 <everettschneider@gmail.com>
2021-06-02 14:27:44 -0500

    Finished scope exercise

--- scope.js
+++ scope.js
@@ -31,15 +31,35 @@ console.log(conjunction("Master", "Card")) */
     console.log(HTMLRepresentation) */
 
 
-const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+/* const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
 
 for (const location of locations) {
 
+    let invalidLocation = false
     if (location[0] > 2) {
-        const invalidLocation = true
+        invalidLocation = true
+        
     }
 
-    if (invalidLocation) {
+    if (invalidLocation === false) {
+        console.log(location[0])
         console.log("This location is invalid")
     }
 }
+ */
+
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        return name + suffix
+    }
+
+    return namer
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker())
